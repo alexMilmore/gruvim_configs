62109559654675b75fe1c6950fea0efc29043699
Alex Milmore <alex.milmore@unipart.io>
2021-02-12 21:21:00 +0000

    Added install for fun command line utils I like

---
+++ fun_stuff_install.sh
@@ -0,0 +1,8 @@
+# hexyl coloured hexdump https://github.com/sharkdp/hexyl
+apt install hexyl
+
+# reddit cli https://github.com/michael-lazar/rtv
+apt install rtv
+
+# jq, basically sed for json
+apt install jq
