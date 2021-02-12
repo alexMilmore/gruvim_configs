c69926325e03c1d6e9a8314e99e3fe7dbc1f0f28
Alex Milmore <alex.milmore@unipart.io>
2021-02-12 21:19:34 +0000

    Added installs for integral programs

---
+++ install_requirements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# urxvt terminal emulator
+apt install rxvt-unicode -y
+
+# terminal multiplexor
+apt install tmux -y
+
+# window manager
+apt install i3 -y
+
+# image viewing
+apt install feh -y
+
+# screenshots
+apt install scrot -y
