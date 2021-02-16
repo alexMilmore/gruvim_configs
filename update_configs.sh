cd003f2dc59beee8b9b2d5b9ca14e4e90840c9d8
Alex Milmore <alex.milmore@unipart.io>
2021-02-16 22:49:53 +0000

    update_configs now works on fresh installs

--- update_configs.sh
+++ update_configs.sh
@@ -3,7 +3,9 @@
 echo "Updating config files"
 
 echo "changing neovim configs"
+mkdir -p ~/.config/nvim
 cp nvim/init.vim ~/.config/nvim/init.vim
+vim +'PlugInstall --sync' +qa
 
 echo "changing git editor to neovim"
 git config --global core.editor "nvim"
@@ -14,6 +16,7 @@ echo "updating .Xresources"
 xrdb ~/.Xresources
 
 echo "copying i3 config (window manager settings)"
+mkdir -p ~/.config/i3
 cp i3/config ~/.config/i3/config
 
 echo "copying tmux config (terminal multiplexor settings)"
